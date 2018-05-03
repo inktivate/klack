c371dcde47c28ee5f320210789cfc23209b4e77e
Ethan Sternke <ethan.sternke@gmail.com>
2018-05-03 13:55:33 -0400

    moved message history to MongoDB

--- app.js
+++ app.js
@@ -1,67 +1,101 @@
-const express = require('express')
+const express = require('express');
 const querystring = require('querystring');
-const port = 3000
-const app = express()
+const port = 3000;
+const app = express();
 
-// List of all messages
-let messages = []
+// MONGOOSE SHIZ
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/klack');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    var messageSchema = mongoose.Schema({
+        sender: String,
+        message: String,
+        timestamp: Number
+    })
 
-// Track last active times for each sender
-let users = {}
+    var Message = mongoose.model('Message', messageSchema);
 
-app.use(express.static("./public"))
-app.use(express.json())
+    // List of all messages
+    let messages = []
 
-// generic comparison function for case-insensitive alphabetic sorting on the name field
-function userSortFn(a, b) {
-    var nameA = a.name.toUpperCase(); // ignore upper and lowercase
-    var nameB = b.name.toUpperCase(); // ignore upper and lowercase
-    if (nameA < nameB) {
-      return -1;
+    // Track last active times for each sender
+    let users = {}
+
+    app.use(express.static("./public"))
+    app.use(express.json())
+
+    // generic comparison function for case-insensitive alphabetic sorting on the name field
+    function userSortFn(a, b) {
+        var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+        var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+        if (nameA < nameB) {
+            return -1;
+        }
+        if (nameA > nameB) {
+            return 1;
+        }
+
+        // names must be equal
+        return 0;
     }
-    if (nameA > nameB) {
-      return 1;
-    }
-  
-    // names must be equal
-    return 0;      
-}
 
-app.get("/messages", (request, response) => {
-    // get the current time
-    const now = Date.now();
+    app.get("/messages", (request, response) => {
+        // get the current time
+        const now = Date.now();
 
-    // consider users active if they have connected (GET or POST) in last 15 seconds
-    const requireActiveSince = now - (15*1000)
+        // consider users active if they have connected (GET or POST) in last 15 seconds
+        const requireActiveSince = now - (15 * 1000)
 
-    // create a new list of users with a flag indicating whether they have been active recently
-    usersSimple = Object.keys(users).map((x) => ({name: x, active: (users[x] > requireActiveSince)}))
+        // create a new list of users with a flag indicating whether they have been active recently
+        usersSimple = Object.keys(users).map((x) => ({ name: x, active: (users[x] > requireActiveSince) }))
 
-    // sort the list of users alphabetically by name
-    usersSimple.sort(userSortFn);
-    usersSimple.filter((a) => (a.name !== request.query.for))
+        // sort the list of users alphabetically by name
+        usersSimple.sort(userSortFn);
+        usersSimple.filter((a) => (a.name !== request.query.for))
 
-    // update the requesting user's last access time
-    users[request.query.for] = now;
+        // update the requesting user's last access time
+        users[request.query.for] = now;
 
-    // send the latest 40 messages and the full user list, annotated with active flags
-    response.send({messages: messages.slice(-40), users: usersSimple})
-})
+        // send the latest 40 messages and the full user list, annotated with active flags
+        // response.send({
+        //     messages: messages.slice(-40),
+        //     users: usersSimple
+        // })
 
-app.post("/messages", (request, response) => {
-    // add a timestamp to each incoming message.
-    const timestamp = Date.now()
-    request.body.timestamp = timestamp
+        // jk do a mongoose
+        Message.find({}, function (err, Message) {
+            response.send({
+                messages: Message.slice(-40),
+                users: usersSimple
+            })
+        });
 
-    // append the new message to the message list
-    messages.push(request.body)
+    })
 
-    // update the posting user's last access timestamp (so we know they are active)
-    users[request.body.sender] = timestamp
+    app.post("/messages", (request, response) => {
+        // add a timestamp to each incoming message.
+        const timestamp = Date.now()
+        request.body.timestamp = timestamp
 
-    // Send back the successful response.
-    response.status(201)
-    response.send(request.body)
-})
+        // append the new message to the message list
+        messages.push(request.body)
 
-app.listen(3000)
+        // MONGOOSE TEST SHIZ
+        let newMessage = new Message(request.body);
+        newMessage.save(function (err, newMessage) {
+            if (err) return console.error(err);
+        })
+
+        // update the posting user's last access timestamp (so we know they are active)
+        users[request.body.sender] = timestamp
+
+        // Send back the successful response.
+        response.status(201)
+        response.send(request.body)
+    })
+
+    app.listen(3000);
+
+});
